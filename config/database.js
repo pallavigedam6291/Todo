8c1dcc02ed508949a84607669e1e16e9519052f6
Tony <pallavigedam0@gmail.com>
2019-03-13 11:26:23 +0530

    second commit

--- config/database.js
+++ config/database.js
@@ -6,11 +6,11 @@ module.exports = function () {
 
     this.con=function () {
         var conn= mysql.createConnection({ 
-            host: '127.0.0.1',
-            user: 'root',
-            password : '',
-            port : 3306,
-            database:'todohuman'
+            host: 'us-cdbr-iron-east-03.cleardb.net',
+            user: 'b7cb37188ad41c',
+            password : '42133918',
+           // port : 3306,
+            database:'heroku_040d6767a44553f'
         });
         return conn;
     }
